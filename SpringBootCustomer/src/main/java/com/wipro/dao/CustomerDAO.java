3feaf3ad1ccc3fe05a065e8514d0aaba8ff1eb3a
ub990 <65628589+ub990@users.noreply.github.com>
2023-07-19 17:29:22 +0530

    Delete SpringBootCustomer directory

--- SpringBootCustomer/src/main/java/com/wipro/dao/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.wipro.bean.Customer;
-
-public interface CustomerDAO extends CrudRepository<Customer,Integer>{
-
-}
